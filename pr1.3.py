cc800a474a4251be24073f37755ead349d0e490b
makeevip@yandex.ru <makeevip@yandex.ru>
2021-02-19 17:49:55 +0300

    correction

--- pr1.3.py
+++ pr1.3.py
@@ -1,12 +1,13 @@
 def f13(n, m):
     s1 = 0
-    for i in range(1, n + 1):
-        for j in range(1, m + 1):
-            s1 += (j ** 8 - 84 * j ** 2 - 40)
     s2 = 0
     for i in range(1, n + 1):
-        s2 += (69 * i + i ** 5)
-    return s1 - 66 * s2
+        for j in range(1, m + 1):
+            s1 += j ** 8 - 84 * j ** 2 - 40
+    for i in range(1, n + 1):
+        s2 += 69 * i + i ** 5
+    s2 = 66 * s2
+    return s1 - s2
 
 
 #print('%.2e' % f13(63, 77))
