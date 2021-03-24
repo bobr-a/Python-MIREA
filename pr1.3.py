cf64aab8e32877c06c06ebc09a3cae76e2fdacdc
makeevip@yandex.ru <makeevip@yandex.ru>
2021-03-24 23:11:29 +0300

    fixed task 1.3

--- pr1.3.py
+++ pr1.3.py
@@ -2,12 +2,12 @@ def f13(n, m):
     s1 = 0
     s2 = 0
     for i in range(1, n + 1):
-        for j in range(1, m + 1):
-            s1 += j ** 8 - 84 * j ** 2 - 40
+        for j in range(1, m+1):
+            s1 += (j ** 8 - 84 * j ** 2 - 40)
     for i in range(1, n + 1):
-        s2 += 69 * i + i ** 5
-    s2 = 66* s2
-    return s1 - s2
+        for j in range(1, m+1):
+            s2 += (69 * i + i ** 5)
+    return s1 - 66 * s2
 
 
-#print('%.2e' % f13(63, 77))
+print(f13(63, 77))
