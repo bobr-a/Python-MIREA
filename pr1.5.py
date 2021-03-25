6bdd35ef672adfe6e41a71e75d4292e81e3f2d62
makeevip@yandex.ru <makeevip@yandex.ru>
2021-03-25 10:05:40 +0300

    add 2.1

---
+++ pr1.5.py
@@ -0,0 +1,41 @@
+def f21(args):
+    if args[4] == "vcl":
+        return 12
+    elif args[4] == "antlr":
+        if args[2] == "xbase":
+            return 11
+        elif args[2] == "mupad":
+            if args[3] == "stata":
+                if args[1] == "haxe":
+                    return 4
+                elif args[1] == "kit":
+                    return 5
+            elif args[3] == "cobol":
+                if args[1] == "haxe":
+                    return 6
+                elif args[1] == "kit":
+                    return 7
+            elif args[3] == "ejs":
+                if args[0] == "perl":
+                    return 8
+                elif args[0] == "tcsh":
+                    return 9
+                elif args[0] == "ampl":
+                    return 10
+        elif args[2] == "less":
+            if args[0] == "perl":
+                return 0
+            elif args [0] == "ampl":
+                return 3
+            elif args[0] == "tcsh":
+                if args[1] == "kit":
+                    return 2
+                elif args[1] == "haxe":
+                    return 1
+
+
+
+
+
+#Makeev
+#print(f14(9))
